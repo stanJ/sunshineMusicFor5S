55389e00589664d3553926cd378e3c069f290c62
blackmatch <blackmatch@1demacbook-pro.local>
2015-10-02 10:28:19 +0800

    Initial Commit

---
+++ LXCSunshineMusicFor5S/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LXCSunshineMusicFor5S
+//
+//  Created by blackmatch on 15/10/2.
+//  Copyright © 2015年 blackmatch. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LXCSunshineMusicFor5S/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LXCSunshineMusicFor5S
+//
+//  Created by blackmatch on 15/10/2.
+//  Copyright © 2015年 blackmatch. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
